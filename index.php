ff9b312c9de4565b7a17b9c00a23b7bb5f6ba20e
usoeficientedeenergia23 <132613282+usoeficientedeenergia23@users.noreply.github.com>
2023-05-05 10:22:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
 
-// Desarrollado por Joystick para todos
-// Julio 2019 - Actualizando
+    // Desarrollado por Joystick para todos
+    // Julio 2019 - Actualizando
 
-// Requerir el archivo de la clase Bee.php
-require_once 'app/classes/Bee.php';
+    // Requerir el archivo de la clase Bee.php
+    require_once 'app/classes/Bee.php';
 
-// Ejecutar el framework bee
-Bee::fly();
+    // Ejecutar el framework bee
+    Bee::fly();
+    ?>
+    
+</body>
+</html>
